d74f7ba9cf938f870c4f0ace77f129829ac3d5e1
Ponss17 <jeancastrogudiel@gmail.com>
2025-11-27 11:54:40 -0600

    Flix

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 const express = require('express');
+const path = require('path');
 const rankRoutes = require('./routes/rank');
 const matchRoutes = require('./routes/match');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Rutas
 app.use('/rank', rankRoutes);
 app.use('/match', matchRoutes);
-
-app.get('/', (req, res) => {
-    res.send('Valorant Rank API is running');
-});
 
 module.exports = app;
