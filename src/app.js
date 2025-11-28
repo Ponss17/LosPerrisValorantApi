4d48397d7b6b92d30e0df9547934536d2c27fcf1
Ponss17 <jeancastrogudiel@gmail.com>
2025-11-28 11:03:21 -0600

    Ajustes de seguridad

--- src/app.js
+++ src/app.js
@@ -11,12 +11,16 @@ const app = express();
 
 app.use(helmet({
     contentSecurityPolicy: {
+        useDefaults: false,
         directives: {
             defaultSrc: ["'self'"],
-            imgSrc: ["'self'", "data:", "https://media.valorant-api.com"],
             scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net"],
             styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
-            fontSrc: ["'self'", "https://fonts.gstatic.com"]
+            fontSrc: ["'self'", "https://fonts.gstatic.com"],
+            imgSrc: ["'self'", "data:", "https://media.valorant-api.com"],
+            connectSrc: ["'self'"],
+            objectSrc: ["'none'"],
+            upgradeInsecureRequests: null
         }
     }
 }));
