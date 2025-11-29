b935fa29e4820ae0e05753aa1baf005046f542d7
Ponss17 <jeancastrogudiel@gmail.com>
2025-11-29 02:02:30 -0600

    Ajuste cache

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,24 +1,8 @@
-const cache = new Map();
+const { LRUCache } = require('lru-cache');
 
-const TTL = 24 * 60 * 60 * 1000;
+const cache = new LRUCache({
+    max: 1000,
+    ttl: 24 * 60 * 60 * 1000,
+});
 
-function get(key) {
-    const item = cache.get(key);
-    if (!item) return null;
-
-    if (Date.now() > item.expiry) {
-        cache.delete(key);
-        return null;
-    }
-
-    return item.value;
-}
-
-function set(key, value, ttl = TTL) {
-    cache.set(key, {
-        value,
-        expiry: Date.now() + ttl
-    });
-}
-
-module.exports = { get, set };
+module.exports = cache;
